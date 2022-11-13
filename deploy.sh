a686b3db6b5ddfd0b5bb3249f3c7de93de427842
linsy king <linsy_king@sjtu.edu.cn>
2022-11-13 21:29:16 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ ssh ta@111.186.58.48 << EOF
   cd /home/ta/22FA/vg151-codequality
   git pull
   echo "pull done"
-  su - root -c "cd /home/ta/22FA/vg151-codequality && cmake --build build --config Release --target codequality && cmake --install build --config Release --target codequality" < ~ta/.p
+  su - root -c "cd /home/ta/22FA/vg151-codequality && cmake --build build --config Release --target codequality && cmake --install build --config Release" < ~ta/.p
 EOF
