94ec4ed031d6d3ec40520e9102755cf2ecd77b1c
linsy king <linsy_king@sjtu.edu.cn>
2022-11-13 21:00:32 +0800

    add script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+ssh ta@111.186.58.48 << EOF
+  cd /home/ta/22FA/vg151-codequality
+  git pull
+  echo "pull done"
+  su - root -c "cd /home/ta/22FA/vg151-codequality && cmake --build build --config Release && cmake --install build --config Release" < ~ta/.p
+EOF
